7f8a522d0fce9a2d4e3837b8bf2ed012a3078c36
Malbongs <qothgus13@naver.com>
2022-12-07 22:31:46 +0900

    text Input canvas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
@@ -16,6 +17,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -87,8 +89,21 @@ function onFileChange(event){
     }
 }
 
+function onDubleClick(event){
+    const text = textInput.value;
+    if(text !== ""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "68px serif"
+        ctx.fillText(text,event.offsetX,event.offsetY);
+        ctx.restore();
+    }
+
+}
 
 
+//text넣기
+canvas.addEventListener("dblclick", onDubleClick);
 //위치잡기
 canvas.addEventListener("mousemove", onMove);
 //선그리기
