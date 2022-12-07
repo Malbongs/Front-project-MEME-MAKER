0cb62bf3583b86e1518fd565dd406c74d71eb467
Malbongs <qothgus13@naver.com>
2022-12-07 21:47:00 +0900

    destroy and eraser

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
@@ -6,8 +8,12 @@ const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 let isFilling = false;
@@ -55,13 +61,31 @@ function onModeClick(){
 
 function onCanvasClick(){
     if(isFilling){
-        ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 }
+
+function onDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerHTML = "Fill";
+}
+
+
+//위치잡기
 canvas.addEventListener("mousemove", onMove);
+//선그리기
 canvas.addEventListener("mousedown",startPainting);
+//선 그리기 취소
 canvas.addEventListener("mouseup",cancelPainting);
+//선 그리기 취소(버그)
 canvas.addEventListener("mouseleave", cancelPainting);
+//canvas 채우기
 canvas.addEventListener("click",onCanvasClick);
 //변경시 함수 실행
 lineWidth.addEventListener("change", onLineWidthChange);
@@ -70,5 +94,9 @@ color.addEventListener("change", onColorChange);
 //컬러클릭시  바뀌는 버튼 
 colorOptions.forEach(color => color.addEventListener("click",onColorClick));
 
-//
+//모드변경
 modeBtn.addEventListener("click", onModeClick);
+//그림판 초기화
+destroyBtn.addEventListener("click",onDestroyClick);
+//지우기
+eraserBtn.addEventListener("click",onEraserClick);
